12979693782c88bac03bcff465e5d8b3b1bca759
yanghu819 <44990984+yanghu819@users.noreply.github.com>
2024-04-21 13:35:44 +0800

    Update run.sh

--- hucfg/run.sh
+++ hucfg/run.sh
@@ -1,5 +1,5 @@
 ####### rm -rf copy-*; 
-python run0.py ## generate the params & copy the exp folders
-cd /sunrui/torus/hucfg/copy-exp##0,1; python main.py;
-cd /sunrui/torus/hucfg/copy-exp##0,2; python main.py;
-
+base_dir="/sunrui/torus/hucfg" ## generate the params & copy the exp folders
+python run0.py
+cd ${base_dir}/copy-exp##0,1; python main.py;
+cd ${base_dir}/copy-exp##0,2; python main.py;
